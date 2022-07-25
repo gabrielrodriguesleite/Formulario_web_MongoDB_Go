6e8816826aa37514bcd5d0490a0afb5e87bf4084
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-25 16:29:22 -0300

    [feat] main go serve frontend/backend

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"FORMULARIO_WEB_MONGODB_GO/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.SubscriptionHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
