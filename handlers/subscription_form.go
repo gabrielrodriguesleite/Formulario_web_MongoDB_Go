eab37c3cb2a51cfbeb9637fe990bb65c5e5ac37d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-25 16:17:37 -0300

    [feat] handler router backend post / frontend serve

---
+++ handlers/subscription_form.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"FORMULARIO_WEB_MONGODB_GO/controllers"
+	"fmt"
+	"net/http"
+)
+
+func SubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+
+	// DEFINE A ROTA POST (em express seria o router.post(...))
+	if r.Method == http.MethodPost {
+
+		// CARREGA OS DADOS DO FORM
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "erro ao fazer parse do form: %v", err)
+			return
+		}
+
+		// chama o controller passando os dados parseados do forms
+		err := controllers.Create(r.PostForm.Get("name"), r.PostForm.Get("email"))
+		if err != nil {
+			fmt.Fprintf(w, "erro ao salvar os dados: %v", err)
+			return
+		}
+	}
+
+	// DEFINE O METODO GET
+	http.ServeFile(w, r, "handlers/templates/subscription_form.html")
+}
